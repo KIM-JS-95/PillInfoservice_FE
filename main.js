bd11b554f7ed14b39c1cccfe9fdeccbb500e1d6e
KIM-JS-95 <baugh248730@gmail.com>
2022-05-03 18:00:47 +0900

    123

--- main.js
+++ main.js
@@ -1,19 +1,97 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
+const {autoUpdater} = require("electron-updater");
+const log = require('electron-log');
 
-function createWindow () {  // 브라우저 창을 생성
-  let win = new BrowserWindow({
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
+//-------------------------------------------------------------------
+// Define the menu
+//
+// THIS SECTION IS NOT REQUIRED
+//-------------------------------------------------------------------
+let template = []
+if (process.platform === 'darwin') {
+  // OS X
+  const name = app.getName();
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: 'About ' + name,
+        role: 'about'
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() { app.quit(); }
+      },
+    ]
+  })
+}
+
+let win;
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
+}
+
+function createDefaultWindow() {  // 브라우저 창을 생성
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true
     }
-  })
+  });
+  win.webContents.openDevTools();
+  win.on('closed', () =>{
+    win = null;
+  });
 
-  //브라우저창이 읽어 올 파일 위치
-  win.loadFile('index.html')
+  win.loadURL(`file://${__dirname}/index.html#v${app.getVersion()}`);
+  return win;
 }
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
 
 app.whenReady()
 .then(() => {
-    createWindow();
+  
+      // Create the Menu
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  createDefaultWindow();
+});
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
+app.on('ready', function()  {
+  autoUpdater.checkForUpdatesAndNotify();
 });
