208c1da6dce5b25036cf3fb6c94ddcf610742237
KIM-JS-95 <baugh248730@gmail.com>
2022-05-04 11:12:11 +0900

    12

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const { app, BrowserWindow, Menu } = require('electron');
-const {autoUpdater} = require("electron-updater");
+var promises_1 = require('fs.promises');
+
+const {app, BrowserWindow, Menu} = require('electron');
 const log = require('electron-log');
+const {autoUpdater} = require("electron-updater");
 
-
+//-------------------------------------------------------------------
+// Logging
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This logging setup is not required for auto-updates to work,
+// but it sure makes debugging easier :)
+//-------------------------------------------------------------------
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
 log.info('App starting...');
@@ -32,30 +41,36 @@ if (process.platform === 'darwin') {
   })
 }
 
+
+//-------------------------------------------------------------------
+// Open a window that displays the version
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This isn't required for auto-updates to work, but it's easier
+// for the app to show a window than to have to click "About" to see
+// that updates are working.
+//-------------------------------------------------------------------
 let win;
 
 function sendStatusToWindow(text) {
   log.info(text);
   win.webContents.send('message', text);
 }
-
-function createDefaultWindow() {  // 브라우저 창을 생성
+function createDefaultWindow() {
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false
     }
   });
   win.webContents.openDevTools();
-  win.on('closed', () =>{
+  win.on('closed', () => {
     win = null;
   });
-
-  win.loadURL(`file://${__dirname}/index.html#v${app.getVersion()}`);
+  win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
   return win;
 }
-
 autoUpdater.on('checking-for-update', () => {
   sendStatusToWindow('Checking for update...');
 })
@@ -77,21 +92,27 @@ autoUpdater.on('download-progress', (progressObj) => {
 autoUpdater.on('update-downloaded', (info) => {
   sendStatusToWindow('Update downloaded');
 });
-
-app.whenReady()
-.then(() => {
-  
-      // Create the Menu
+app.on('ready', function() {
+  // Create the Menu
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
 
   createDefaultWindow();
 });
-
 app.on('window-all-closed', () => {
   app.quit();
 });
 
+//
+// CHOOSE one of the following options for Auto updates
+//
+
+//-------------------------------------------------------------------
+// Auto updates - Option 1 - Simplest version
+//
+// This will immediately download an update, then install when the
+// app quits.
+//-------------------------------------------------------------------
 app.on('ready', function()  {
   autoUpdater.checkForUpdatesAndNotify();
 });
